7204b9c109bf85d6ceb9215ff4133089f063b329
Luiz Alberto <kingpin@192.168.0.32>
2026-04-04 15:05:52 -0300

    test: hardcode dashboard unit id

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,8 @@
-export default function AdminPage() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>ADMIN OK</h1>
-    </div>
-  );
+import { DashboardView } from "@/components/admin/dashboard-view";
+import { getDashboardData } from "@/lib/queries/admin";
+
+export default async function AdminDashboardPage() {
+  const data = await getDashboardData("34249606-e36d-4328-b899-5233c0329d17");
+
+  return <DashboardView data={data} />;
 }
